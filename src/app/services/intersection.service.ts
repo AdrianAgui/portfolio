2d68a18a06ac07857edf7a6d491126b27a3afc21
AdriAgui <adrian.agui9@gmail.com>
2022-07-06 21:23:16 +0200

    some animations

---
+++ src/app/services/intersection.service.ts
@@ -0,0 +1,25 @@
+import { ElementRef, Injectable } from '@angular/core';
+import { distinctUntilChanged, map, mergeMap, Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class IntersectionService {
+  createAndObserve(element: ElementRef): Observable<boolean> {
+    return new Observable((observer) => {
+      const intersectionObserver = new IntersectionObserver((entries) => {
+        observer.next(entries);
+      });
+
+      intersectionObserver.observe(element.nativeElement);
+
+      return () => {
+        intersectionObserver.disconnect();
+      };
+    }).pipe(
+      mergeMap((entries: any) => entries),
+      map((entry: any) => entry.isIntersecting),
+      distinctUntilChanged()
+    );
+  }
+}
